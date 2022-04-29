08c16a40b925917f1dbe5acc12bccc21ca133e5a
Micah <micahnyoung95@gmail.com>
2022-04-28 21:01:57 -0500

    finished studio

--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -1,6 +1,8 @@
 package test;
 
+import main.BalancedBrackets;
 import org.junit.Test;
+//import main.
 
 import static org.junit.Assert.*;
 
@@ -11,6 +13,58 @@ public class BalancedBracketsTest {
     public void emptyTest() {
         assertEquals(true, true);
     }
+
+    @Test
+    public void onlyBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
+    }
+
+    @Test
+    public void stringBetweenBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[string]"));
+    }
+
+    @Test
+    public void bracketsContainsBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[[]]"));
+    }
+
+    @Test
+    public void stringBeforeBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("String[]"));
+    }
+
+    @Test
+    public void bracketsBeforeAndAfterBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("String[]String"));
+    }
+
+    @Test
+    public void emptyStringReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets(""));
+    }
+
+    @Test
+    public void hasClosingBracketFirstReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]["));
+    }
+
+    @Test
+    public void stringThenClosedThenStringThenOpenReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("Launch]Code["));
+    }
+
+    @Test
+    public void noClosingBracketReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[LaunchCode"));
+    }
+
+
+
+
+
+
+
 
 
 }
